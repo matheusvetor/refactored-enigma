511b656116ff17a5e9a8ccac95c8632d67ead1ec
Matheus Oliveira <matheus.vetor@gmail.com>
2017-12-28 14:22:48 -0200

    change app name

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'action_view/railtie'
 
 Bundler.require(*Rails.groups)
 
-module FractalTest
+module RefactoredEnigma
   class Application < Rails::Application
     config.load_defaults 5.1
 
