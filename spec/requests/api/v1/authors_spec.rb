30f746d9d87c4af178fa9dcb03c62ec30ea34c44
Matheus Oliveira <matheus.vetor@gmail.com>
2017-12-31 12:40:02 -0200

    make 100% code coverage

--- spec/requests/api/v1/authors_spec.rb
+++ spec/requests/api/v1/authors_spec.rb
@@ -71,5 +71,18 @@ RSpec.describe 'Author API', type: :request do
         expect(response).to have_http_status(201)
       end
     end
+
+    context 'when the request is invalid' do
+      before { post '/api/v1/authors', params: { name: nil } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a validation failure message' do
+        expect(response.body)
+          .to match(/Name can't be blank/)
+      end
+    end
   end
 end
