84f3734b18833ce87ae92ba840b91ba40e213f15
Matheus Oliveira <matheus.vetor@gmail.com>
2018-01-02 17:54:05 -0200

    Putting back sidekiq configuration

--- config/puma.rb
+++ config/puma.rb
@@ -6,12 +6,3 @@ port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 plugin :tmp_restart
-
-on_worker_boot do
-  @sidekiq_pid ||= spawn('bundle exec sidekiq -t 1')
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-end
-
-on_restart do
-  Sidekiq.redis.shutdown { |conn| conn.close }
-end
